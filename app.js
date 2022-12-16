d2ec523772a8620c0e489065db17348548574e1e
Giovanni Bedoya <giovedoya@gmail.com>
2022-12-16 23:24:17 +0100

    'Beerpartial'

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Register the location for handlebars partials here:
 
 // ...
