8607de44021c9eb27f57a1e276bdeedfe5307c69
Giovanni Bedoya <giovedoya@gmail.com>
2023-01-09 22:04:16 +0100

    'Final'

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-const express = require('express');
+const express = require('express')
 
-const hbs = require('hbs');
-const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const hbs = require('hbs')
+const path = require('path')
+const PunkAPIWrapper = require('punkapi-javascript-wrapper')
 
+const app = express()
+const punkAPI = new PunkAPIWrapper()
 
-
-const app = express();
-const punkAPI = new PunkAPIWrapper();
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
 
 app.get('/', (req, res) => {
-  res.status(200).render('index');
-});
+  res.status(200).render('index')
+})
 
-app.get('/beers', (req, res, next) => { 
-  punkAPI 
-  .getBeers()
-  .then(beersFromApi => {
-    res.status(200).render('beers', {beersFromApi});
-  })
-    .catch(error => console.log(error));  
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.status(200).render('beers', { beersFromApi })
+    })
+    .catch(error => console.log(error))
 })
 
 app.get('/random-beer', (req, res, next) => {
   punkAPI
-  .getRandom()
-  .then(responseFromAPI => {
-    res.status(200).render('random-beer', {responseFromAPI})
-  })
-  .catch(error => console.log(error));  
+    .getRandom()
+    .then(responseFromAPI => {
+      res.status(200).render('random-beer', { responseFromAPI })
+    })
+    .catch(error => console.log(error))
 })
 
-
-
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
